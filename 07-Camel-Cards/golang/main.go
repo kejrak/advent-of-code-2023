12a0a231ae0e2188fbf68d0f5399838f3b8a8342
Jan Kejř <jan.kejr@centrum.cz>
2023-12-09 12:42:53 +0100

    first part refactor

--- 07-Camel-Cards/golang/main.go
+++ 07-Camel-Cards/golang/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
@@ -22,169 +23,52 @@ type HandCards struct {
 	Strength int
 }
 
+func mapValues(a map[string]int) []int {
+	x := []int{}
+	for _, val := range a {
+		x = append(x, val)
+	}
+
+	return x
+}
+
+func countCards(hand string) []int {
+	c := make(map[string]int)
+
+	for _, card := range hand {
+		char := string(card)
+		if _, ok := c[char]; ok {
+			c[char] += 1
+		} else {
+			c[char] = 1
+		}
+	}
+
+	values := mapValues(c)
+	slices.Sort(values)
+	slices.Reverse(values)
+	return values
+}
+
 func (h *HandCards) GetType() {
+	counts := countCards(h.Cards)
 
-	occurrences := countCardOccurrences(h.Cards)
-
-	isFiveStrength, isFive := h.isFive(occurrences)
-	isFourStrength, isFour := h.isFour(occurrences)
-	isFullHouseStrength, isFullHouse := h.isFullHouse(occurrences)
-	isThreeStrength, isThree := h.isThree(occurrences)
-	isTwoStrength, isTwo := h.isTwo(occurrences)
-	isOneStrength, isOne := h.isOne(occurrences)
-	isHighStrength, isHigh := h.isHigh(occurrences)
-
-	if isFive {
-		h.Strength = isFiveStrength
+	if counts[0] == 5 {
+		h.Strength = 7
+	} else if counts[0] == 4 {
+		h.Strength = 6
+	} else if counts[0] == 3 && counts[1] == 2 {
+		h.Strength = 5
+	} else if counts[0] == 3 {
+		h.Strength = 4
+	} else if counts[0] == 2 && counts[1] == 2 {
+		h.Strength = 3
+	} else if counts[0] == 2 {
+		h.Strength = 2
+	} else {
+		h.Strength = 1
 	}
 
-	if isFour {
-		h.Strength = isFourStrength
-	}
-
-	if isFullHouse {
-		h.Strength = isFullHouseStrength
-	}
-
-	if isThree {
-		h.Strength = isThreeStrength
-	}
-
-	if isTwo {
-		h.Strength = isTwoStrength
-	}
-
-	if isOne {
-		h.Strength = isOneStrength
-	}
-
-	if isHigh {
-		h.Strength = isHighStrength
-	}
-
-}
-
-func (h *HandCards) isFive(occurrences map[rune]int) (int, bool) {
-
-	for _, count := range occurrences {
-
-		if count == 5 {
-
-			return 7, true
-
-		}
-	}
-	return 0, false
-
-}
-
-func (h *HandCards) isFour(occurrences map[rune]int) (int, bool) {
-
-	for _, count := range occurrences {
-
-		if count == 4 {
-
-			return 6, true
-
-		}
-	}
-	return 0, false
-}
-
-func (h *HandCards) isFullHouse(occurrences map[rune]int) (int, bool) {
-
-	threeCount := 0
-	twoCount := 0
-
-	for _, count := range occurrences {
-
-		switch count {
-		case 3:
-			threeCount++
-		case 2:
-			twoCount++
-		}
-	}
-	return 5, threeCount == 1 && twoCount == 1
-}
-
-func (h *HandCards) isThree(occurrences map[rune]int) (int, bool) {
-
-	threeCount := 0
-	oneCount := 0
-
-	for _, count := range occurrences {
-
-		switch count {
-		case 3:
-			threeCount++
-		case 1:
-			oneCount++
-		}
-	}
-
-	return 4, threeCount == 1 && oneCount == 2
-
-}
-
-func (h *HandCards) isTwo(occurrences map[rune]int) (int, bool) {
-
-	twoCount := 0
-
-	for _, count := range occurrences {
-
-		if count == 2 {
-			twoCount++
-		}
-	}
-
-	return 3, twoCount == 2
-
-}
-
-func (h *HandCards) isOne(occurrences map[rune]int) (int, bool) {
-
-	twoCount := 0
-	oneCount := 0
-
-	for _, count := range occurrences {
-
-		switch count {
-		case 1:
-			oneCount++
-		case 2:
-			twoCount++
-		}
-	}
-
-	return 2, twoCount == 1 && oneCount == 3
-
-}
-
-func (h *HandCards) isHigh(occurrences map[rune]int) (int, bool) {
-
-	oneCount := 0
-
-	for _, count := range occurrences {
-
-		if count == 1 {
-			oneCount++
-		}
-
-	}
-	return 1, oneCount == 5
-}
-
-func countCardOccurrences(s string) map[rune]int {
-
-	cardCount := make(map[rune]int)
-
-	for _, card := range s {
-
-		cardCount[card]++
-	}
-
-	return cardCount
 }
 
 func getLines(filePath string) ([]string, error) {
