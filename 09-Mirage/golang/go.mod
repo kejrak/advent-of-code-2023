8e1516de5d2ee295ee56027326edcaba8d5e4640
Jan Kejř <jan.kejr@centrum.cz>
2023-12-09 17:33:20 +0100

    09-Mirage

---
+++ 09-Mirage/golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/kejrak/advent-of-code-2023/09-Mirage
+
+go 1.21.4
---
+++ 09-Mirage/golang/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getLines(filePath string) ([]string, error) {
+	readFile, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(readFile), "\n")
+	return lines, err
+}
+
+func GetSum(lastValues []int) int {
+	sum := 0
+	add := 0
+	i := 0
+
+	for i < len(lastValues)-1 {
+		add = sum + lastValues[i+1]
+		sum = add
+
+		i += 1
+	}
+
+	return sum
+}
+
+func checkZeros(values []int) bool {
+
+	for _, value := range values {
+		if value != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IterateOverSlice(values []int, lastValue int, lastValues []int) []int {
+	var result []int
+
+	if checkZeros(values) {
+		return lastValues
+	}
+
+	i := 0
+
+	for i < len(values)-1 {
+
+		diff := values[i+1] - values[i]
+
+		result = append(result, diff)
+		i += 1
+	}
+
+	lastValue = result[len(result)-1]
+	lastValues = append(lastValues, lastValue)
+
+	return IterateOverSlice(result, lastValue, lastValues)
+}
+
+func PartOne(data []string) int {
+
+	var PartOne int
+	for _, line := range data {
+		var listOfLastValues []int
+
+		values := strings.Split(line, " ")
+		intValues := make([]int, len(values))
+
+		for i, s := range values {
+			intValues[i], _ = strconv.Atoi(s)
+		}
+
+		listOfLastValues = IterateOverSlice(intValues, intValues[len(intValues)-1], []int{})
+
+		for i, j := 0, len(listOfLastValues)-1; i < j; i, j = i+1, j-1 {
+			listOfLastValues[i], listOfLastValues[j] = listOfLastValues[j], listOfLastValues[i]
+		}
+
+		listOfLastValues = append(listOfLastValues, intValues[len(intValues)-1])
+
+		getSum := GetSum(listOfLastValues)
+
+		PartOne += getSum
+	}
+
+	return PartOne
+
+}
+
+func PartTwo(data []string) int {
+
+	var PartTwo int
+	for _, line := range data {
+		var listOfLastValues []int
+
+		values := strings.Split(line, " ")
+		intValues := make([]int, len(values))
+
+		for i, s := range values {
+			intValues[i], _ = strconv.Atoi(s)
+		}
+
+		for i, j := 0, len(intValues)-1; i < j; i, j = i+1, j-1 {
+			intValues[i], intValues[j] = intValues[j], intValues[i]
+		}
+
+		listOfLastValues = IterateOverSlice(intValues, intValues[len(intValues)-1], []int{})
+
+		for i, j := 0, len(listOfLastValues)-1; i < j; i, j = i+1, j-1 {
+			listOfLastValues[i], listOfLastValues[j] = listOfLastValues[j], listOfLastValues[i]
+		}
+
+		listOfLastValues = append(listOfLastValues, intValues[len(intValues)-1])
+
+		getSum := GetSum(listOfLastValues)
+
+		PartTwo += getSum
+	}
+
+	return PartTwo
+
+}
+
+func main() {
+	data, err := getLines("./input.txt")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Printf("Part Two: %d\n", PartOne(data))
+	fmt.Printf("Part Two: %d\n", PartTwo(data))
+
+}
